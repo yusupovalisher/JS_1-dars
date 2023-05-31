e2632892fba77b1fe122df390deeaa9216ec14c7
yusupovalisher <volk8489@gmail.com>
2023-05-31 12:57:44 +0500

    Bu  JS1-dars kodlari

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+/*document.write("Men bodiyda ko'rinaman!")*/
+
+/*let x=2;*/
+/*let y=5;*/
+/*x=10*/
+/*let x=2,y=5,z=10;*/
+
+/*let color = "#f1f1f1"*/
+/*let salom = "Assalomu aleykum" */
+/*document.write(color);*/
+
+/* data types: numer,string */
+
+/*let  x=5;*/
+/*let soz = "JavaScript";*/
+/*document.write(typeof soz);*/
+
+/*JS arifmetik hisoblashlar*/
+
+/*let x=5;*/
+/*x+=5;*//* x=5+5 */
+/*x-=2;*//* x=5-2 */
+/*x*=5;*//* x=5*5 */
+/*x/=2; */ /* x=5/2 */
+/*let qoldiq = x%4;*/
+/*let daraja = x**5*/
+/*++x;*/
+/*--x;*/
+/*document.write(x*"matn"*/)
+
+/* if => Agar */
+/* else = u holda */
+
+/*if ("Salom" == "Salo"){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!")
+}*/
+
+/* if => agar */
+/* else if => yoki bolmasa*/
+/* else => u holda*/
+
+if (45 == 46){
+    console.log("Liop the holetily")
+}
+else if (45 == 45){
+    console.log("Endi bu son 45ga teng")
+}
+else{
+    console.log("Ekkala son 45ga teng emas")
+}
